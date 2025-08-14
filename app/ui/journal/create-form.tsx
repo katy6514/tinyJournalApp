c4c7fc4694274c14860dd3f258652c69fd0b5a55
Katherine Wyman <katy6514@gmail.com>
2025-08-14 14:42:20 -0700

    Form edits and client side validation

--- app/ui/journal/create-form.tsx
+++ app/ui/journal/create-form.tsx
@@ -14,18 +14,19 @@ export default function Form({
     <form action={createEntry}>
       <div className=" bg-gray-50 p-4 md:p-6">
         <div className="grid gap-6 mb-6 md:grid-cols-2">
-          {/* Select an empty date */}
+          {/* DATE SELECTION */}
           <div>
             <label
               htmlFor="date"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Select an date
+              Select an empty date
             </label>
             <select
               id="date"
               name="date_id"
               className="bg-white border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
             >
               <option value="" disabled>
                 Select a date
@@ -38,7 +39,7 @@ export default function Form({
             </select>
           </div>
 
-          {/* Select a state */}
+          {/* STATE */}
           <div>
             <label
               htmlFor="state"
@@ -47,34 +48,34 @@ export default function Form({
               State
             </label>
             <input
-              type="text"
               id="state"
               name="state"
+              type="text"
+              placeholder="NM/CO/WY/ID/MT"
               className="bg-white border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Doe"
               required
             />
           </div>
 
-          {/* Title / Legname */}
+          {/* LEGNAME */}
           <div className="mb-6">
             <label
               htmlFor="legname"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Title / LegName
+              LegName
             </label>
             <input
               type="text"
               id="legname"
               name="legname"
               className="bg-white border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="john.doe@company.com"
+              placeholder="Title"
               required
             />
           </div>
 
-          {/* Journal Entry */}
+          {/* JOURNAL ENTRY */}
           <div className="mb-6">
             <label
               htmlFor="entryText"
@@ -87,6 +88,7 @@ export default function Form({
               name="entryText"
               className="block p-2.5 w-full text-sm text-gray-900 bg-white  border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Write your thoughts here..."
+              required
             ></textarea>
           </div>
         </div>
