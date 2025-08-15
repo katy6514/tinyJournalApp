44477acfd1bc71bd34694d505c61804df36855ca
Katherine Wyman <katy6514@gmail.com>
2025-08-15 09:28:04 -0700

    stylings done for create form

--- app/ui/journal/create-form.tsx
+++ app/ui/journal/create-form.tsx
@@ -21,9 +21,9 @@ export default function Form({
   return (
     <form action={formAction}>
       <div className=" bg-gray-50 p-4 md:p-6">
-        <div className="grid gap-6 mb-6 md:grid-cols-2">
+        <div className="grid gap-6 mb-6 grid-cols-2 grid-rows-4">
           {/* DATE SELECTION */}
-          <div>
+          <div className="col-span-2 md:col-span-1  row-span-1">
             <label
               htmlFor="date"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -44,18 +44,18 @@ export default function Form({
                 </option>
               ))}
             </select>
-          </div>
-          <div id="date-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.date_id &&
-              state.errors.date_id.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
+            <div id="date-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.date_id &&
+                state.errors.date_id.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
           </div>
 
           {/* STATE */}
-          <div>
+          <div className="col-span-2 md:col-span-1  row-span-1">
             <label
               htmlFor="state"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -71,18 +71,18 @@ export default function Form({
               aria-describedby="state-error"
               // required
             />
-          </div>
-          <div id="state-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.state &&
-              state.errors.state.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
+            <div id="state-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.state &&
+                state.errors.state.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
           </div>
 
           {/* LEGNAME */}
-          <div className="mb-6">
+          <div className="col-span-2 row-span-1">
             <label
               htmlFor="legname"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -98,18 +98,18 @@ export default function Form({
               aria-describedby="legname-error"
               // required
             />
-          </div>
-          <div id="legname-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.legname &&
-              state.errors.legname.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
+            <div id="legname-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.legname &&
+                state.errors.legname.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
           </div>
 
           {/* JOURNAL ENTRY */}
-          <div className="mb-6">
+          <div className="col-span-2 row-span-2">
             <label
               htmlFor="entryText"
               className="block mb-2 w-full text-sm font-medium text-gray-900 dark:text-white"
@@ -119,19 +119,19 @@ export default function Form({
             <textarea
               id="entryText"
               name="entryText"
-              className="block p-2.5 w-full text-sm text-gray-900 bg-white  border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block p-2.5 w-full h-full text-sm text-gray-900 bg-white  border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Write your thoughts here..."
               aria-describedby="entry-error"
               // required
             ></textarea>
-          </div>
-          <div id="entry-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.text &&
-              state.errors.text.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
+            <div id="entry-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.text &&
+                state.errors.text.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
           </div>
         </div>
       </div>
