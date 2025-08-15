5fd711f89a59b75bdc3598b5c543d4dbe42215b3
Katherine Wyman <katy6514@gmail.com>
2025-08-15 09:08:21 -0700

    server side validation working

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -11,23 +11,43 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 const FormSchema = z.object({
   date: z.string(),
   id: z.string(),
-  date_id: z.coerce.number(),
-  text: z.string(),
-  legname: z.string(),
-  state: z.string(),
+  date_id: z.string().min(1, { message: "Please select a date." }),
+  text: z.string().min(1, { message: "Please enter some text." }),
+  legname: z.string().min(1, { message: "Please enter a title or leg name." }),
+  state: z.string().min(1, { message: "Please select a state." }),
 });
 
 const CreateEntry = FormSchema.omit({ date: true, id: true });
-
 const UpdateEntry = FormSchema.omit({ id: true, date: true, date_id: true });
 
-export async function createEntry(formData: FormData) {
-  const { date_id, state, legname, text } = CreateEntry.parse({
+export type State = {
+  errors?: {
+    date_id?: string[];
+    legname?: string[];
+    state?: string[];
+    text?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createEntry(prevState: State, formData: FormData) {
+  const validatedFields = CreateEntry.safeParse({
     date_id: formData.get("date_id"),
     state: formData.get("state"),
     legname: formData.get("legname"),
     text: formData.get("entryText"),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to create entry.",
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { date_id, legname, state, text } = validatedFields.data;
 
   try {
     await sql`
--- app/ui/journal/create-form.tsx
+++ app/ui/journal/create-form.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import { useActionState } from "react";
+
 import Link from "next/link";
 
 import { Button } from "@/app/ui/button";
-import { createEntry } from "@/app/lib/actions";
+import { createEntry, State } from "@/app/lib/actions";
 import { JournalEntry } from "@/app/lib/definitions";
 
 export default function Form({
@@ -9,9 +13,13 @@ export default function Form({
 }: {
   emptyEntries: JournalEntry[];
 }) {
+  const initialState: State = { message: null, errors: {} };
+
+  const [state, formAction] = useActionState(createEntry, initialState);
+
   //   console.log({ emptyEntries });
   return (
-    <form action={createEntry}>
+    <form action={formAction}>
       <div className=" bg-gray-50 p-4 md:p-6">
         <div className="grid gap-6 mb-6 md:grid-cols-2">
           {/* DATE SELECTION */}
@@ -26,17 +34,24 @@ export default function Form({
               id="date"
               name="date_id"
               className="bg-white border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
+              aria-describedby="date-error"
+              // required
             >
-              <option value="" disabled>
-                Select a date
-              </option>
+              <option value="">Select a date</option>
               {emptyEntries.map((entry) => (
                 <option key={entry.date_id} value={entry.date_id}>
                   {entry.date}
                 </option>
               ))}
             </select>
+          </div>
+          <div id="date-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.date_id &&
+              state.errors.date_id.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
 
           {/* STATE */}
@@ -53,8 +68,17 @@ export default function Form({
               type="text"
               placeholder="NM/CO/WY/ID/MT"
               className="bg-white border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
+              aria-describedby="state-error"
+              // required
             />
+          </div>
+          <div id="state-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.state &&
+              state.errors.state.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
 
           {/* LEGNAME */}
@@ -71,8 +95,17 @@ export default function Form({
               name="legname"
               className="bg-white border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Title"
-              required
+              aria-describedby="legname-error"
+              // required
             />
+          </div>
+          <div id="legname-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.legname &&
+              state.errors.legname.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
 
           {/* JOURNAL ENTRY */}
@@ -88,8 +121,17 @@ export default function Form({
               name="entryText"
               className="block p-2.5 w-full text-sm text-gray-900 bg-white  border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Write your thoughts here..."
-              required
+              aria-describedby="entry-error"
+              // required
             ></textarea>
+          </div>
+          <div id="entry-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.text &&
+              state.errors.text.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
       </div>
