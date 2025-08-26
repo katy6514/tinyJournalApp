de44a3e61e6fccdf427dbdc92894ea9f9391790d
Katherine Wyman <katy6514@gmail.com>
2025-08-25 18:57:09 -0700

    fix login photo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 import LoginForm from "@/app/ui/login-form";
 import { Suspense } from "react";
+import titcombBasin from "../../public/titcombBasin.jpg";
 
 export default function LoginPage() {
   return (
     <main className="flex flex-column justify-items-center, items-center">
       <div className="basis-1/4 ">
         <Image
-          src="/titcombBasin.jpg"
+          src={titcombBasin}
           width={394}
           height={700}
           alt="Photo of Katy nearing the top of a mountain pass in the Wind River Range near sunrise, many mountains and lakes behind her"
