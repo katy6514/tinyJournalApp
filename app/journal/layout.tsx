1020430fa764e95198a73b07e5d5f3823c246270
Katherine Wyman <katy6514@gmail.com>
2026-04-25 17:12:09 -0600

    make map take up the whole screen

--- app/journal/layout.tsx
+++ app/journal/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col">
       <Navigation />
-      <div className="flex-grow overflow-y-auto p-6 md:p-12">{children}</div>
+      <div className="flex-grow overflow-y-auto">{children}</div>
     </div>
   );
 }
