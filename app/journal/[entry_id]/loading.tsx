ce7f5aa7c73dd82d1b4c4c09ead5da9dccc73311
Katherine Wyman <katy6514@gmail.com>
2026-05-31 22:48:02 -0600

    Update loading.tsx

--- app/journal/[entry_id]/loading.tsx
+++ app/journal/[entry_id]/loading.tsx
@@ -6,29 +6,29 @@ export default function Loading() {
       <BreadcrumbSkeleton />
 
       {/* Prev / Next navigation */}
-      <div className="flex justify-between py-2 text-sm w-[85%] mx-auto">
-        <div className="skeleton h-4 w-40" />
-        <div className="skeleton h-4 w-40" />
+      <div className="flex justify-between py-2 mb-2 w-[85%] mx-auto">
+        <div className="skeleton h-8 w-36 rounded-lg" />
+        <div className="skeleton h-8 w-36 rounded-lg" />
       </div>
 
-      {/* Metadata + minimap section */}
-      <div className="flex gap-6 items-start py-6 w-[85%] mx-auto">
+      {/* Metadata + minimap hero card */}
+      <div className="flex gap-6 items-start p-6 w-[85%] mx-auto bg-white dark:bg-gray-700 rounded-lg shadow-sm">
         <div className="w-1/2 flex flex-col items-end space-y-1">
           <div className="skeleton h-5 w-40" />
           <div className="skeleton h-7 w-48" />
           <div className="skeleton h-6 w-28" />
-          <div className="space-y-1.5 pt-3 flex flex-col items-end">
+          <div className="space-y-1.5 pt-3 flex flex-col items-start">
             <div className="flex gap-4">
-              <div className="skeleton h-4 w-12" />
-              <div className="skeleton h-4 w-32" />
+              <div className="skeleton h-5 w-16" />
+              <div className="skeleton h-5 w-36" />
             </div>
             <div className="flex gap-4">
-              <div className="skeleton h-4 w-12" />
-              <div className="skeleton h-4 w-32" />
+              <div className="skeleton h-5 w-16" />
+              <div className="skeleton h-5 w-36" />
             </div>
             <div className="flex gap-4">
-              <div className="skeleton h-4 w-16" />
-              <div className="skeleton h-4 w-16" />
+              <div className="skeleton h-5 w-20" />
+              <div className="skeleton h-5 w-20" />
             </div>
           </div>
         </div>
@@ -37,19 +37,21 @@ export default function Loading() {
         </div>
       </div>
 
+      <hr className="w-[85%] mx-auto mt-8 border-gray-200 dark:border-gray-600" />
+
       {/* Constrained journal content */}
       <div className="max-w-3xl mx-auto p-6">
-        <div className="space-y-2">
-          <div className="skeleton h-4 w-full" />
-          <div className="skeleton h-4 w-11/12" />
-          <div className="skeleton h-4 w-full" />
-          <div className="skeleton h-4 w-10/12" />
-          <div className="skeleton h-4 w-full" />
-          <div className="skeleton h-4 w-11/12" />
-          <div className="skeleton h-4 w-9/12" />
-          <div className="skeleton h-4 w-full" />
-          <div className="skeleton h-4 w-10/12" />
-          <div className="skeleton h-4 w-7/12" />
+        <div className="space-y-3 mt-8">
+          <div className="skeleton h-5 w-full" />
+          <div className="skeleton h-5 w-11/12" />
+          <div className="skeleton h-5 w-full" />
+          <div className="skeleton h-5 w-10/12" />
+          <div className="skeleton h-5 w-full" />
+          <div className="skeleton h-5 w-11/12" />
+          <div className="skeleton h-5 w-9/12" />
+          <div className="skeleton h-5 w-full" />
+          <div className="skeleton h-5 w-10/12" />
+          <div className="skeleton h-5 w-7/12" />
         </div>
         <div className="flex gap-1 mt-8">
           <div className="skeleton h-28 flex-[2]" />
